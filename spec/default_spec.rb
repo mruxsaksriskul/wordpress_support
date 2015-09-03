398c826df29f610176ef4ff6b25c7879ef6f52e9
JayKHicks <jhicks@gmti.gannett.com>
2015-09-03 17:09:59 -0400

    updated the and_return to be proper yaml. not sure it needs to be.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -14,15 +14,15 @@ describe 'chef-wordpresswrapper' do
       node.set['ssh_keys'] = ''
       node.default['ssh_keys'] = { test: ['test'] }
       node.set['users']['teams'] = ['test']
-      stub_data_bag_item('users', 'test').and_return(id: 'test', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('users', 'fhanson').and_return(id: 'fhanson', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('users', 'rgindes').and_return(id: 'rgindes', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('users', 'jneves').and_return(id: 'jneves', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('users', 'jcrawford').and_return(id: 'jcrawford', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('users', 'blieberman').and_return(id: 'blieberman', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('users', 'akemner').and_return(id: 'akemner', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('users', 'nessus').and_return(id: 'nessus', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test')
-      stub_data_bag_item('wordpress', 'greenbay').and_return(id: 'greenbay' ,site_name: 'site', db_name: 'db', db_user: 'dbuser', db_password: 'derp')
+      stub_data_bag_item('users', 'test').and_return({ id: 'test', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('users', 'fhanson').and_return({ id: 'fhanson', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('users', 'rgindes').and_return({ id: 'rgindes', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('users', 'jneves').and_return({ id: 'jneves', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('users', 'jcrawford').and_return({ id: 'jcrawford', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('users', 'blieberman').and_return({ id: 'blieberman', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('users', 'akemner').and_return({ id: 'akemner', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('users', 'nessus').and_return({ id: 'nessus', team: 'test', ssh_keys: 'derp_key', administrator: 'true', group: 'test' })
+      stub_data_bag_item('wordpress', 'greenbay').and_return({ id: 'greenbay' ,site_name: 'site', db_name: 'db', db_user: 'dbuser', db_password: 'derp' })
       stub_command('test -d /etc/php-fpm.d || mkdir -p /etc/php-fpm.d').and_return('')
     end.converge(described_recipe)
   end
